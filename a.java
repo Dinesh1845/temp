50b0c97dec592ac3f2a0813fc2afd3454b82b4bd
DineshShankar <dineshshankar156@gmail.com>
2024-11-05 22:27:33 +0530

    m

--- a.java
+++ a.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
 public class a {
     public static void main(String[] args) {
-        int[] n = {1,2,3,4};
+        Scanner a = new Scanner(System.in);
+        System.out.print("enter a values :");
+        
+        int input = a.nextInt();
+
+        int[] b = new int[input];
+        System.out.println("enter next values :");
+        for(int i = 0; i < input; i++){
+            b[i] = a.nextInt();
+        }
+
+
         int sum = 0;
 
-        for(int i = 0; i<n.length; i++){
-            sum = sum+ n[i];
-
+        for(int i = 0; i<b.length; i++){
+            sum+= b[i];
 
         }
         System.out.println(sum);
+
+
+        
     }
 }

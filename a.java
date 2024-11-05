ef80b722fcdf1e745a8fbb869521757d0eec6abe
DineshShankar <dineshshankar156@gmail.com>
2024-11-05 20:59:04 +0530

    sum of array

---
+++ a.java
@@ -0,0 +1,13 @@
+public class a {
+    public static void main(String[] args) {
+        int[] n = {1,2,3,4};
+        int sum = 0;
+
+        for(int i = 0; i<n.length; i++){
+            sum = sum+ n[i];
+
+
+        }
+        System.out.println(sum);
+    }
+}
